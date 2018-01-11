4d072c64765fb1cbd0b434d6722b6caf1e3ca154
michaelrhodes <spam@michaelrhod.es>
2018-01-11 22:19:36 +1100

    Use my browser-pack-flat fork

--- bin.js
+++ bin.js
@@ -1,27 +1,11 @@
 #!/usr/bin/env node
 
-var path = require('path')
 var browserify = require('browserify')
-var find = require('find-pkg')
 var tinyify = require('./plugin/tinyify')
 
 var entry = process.argv[2]
 var standalone = /^(-s|--standalone)$/.test(process.argv[3])
-var name = process.argv[4]
-var opts = {
-  entries: [entry],
-  plugin: [tinyify],
-}
+var opts = { entries: [entry], plugin: [tinyify] }
+if (standalone) opts.standalone = 'x'
 
-if (standalone) {
-  opts.standalone = name || lookup(entry)
-}
-
-browserify(opts)  
-  .bundle()
-  .pipe(process.stdout)
-
-function lookup (file, pkg) {
-  return (pkg = find.sync(file)) ?
-    require(pkg).name : 'bundle'
-}
+browserify(opts).bundle().pipe(process.stdout)
